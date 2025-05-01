95d8a831071ab53ebdbf7211d1a79667f05c2196
Nur-Alam-Limon <nuralam201425@gmail.com>
2025-05-01 11:44:14 +0600

    Updated postman and resolved few issues

--- src/module/admin/admin.route.ts
+++ src/module/admin/admin.route.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { authorizeRoles } from "../../middleware/jwtToken";
+import { authorizeRoles, verifyToken } from "../../middleware/jwtToken";
 import { getDashboardStats } from "./admin.controller";
 
 
 const router = express.Router();
 
-router.get('/dashboard', authorizeRoles('admin'), getDashboardStats);
+router.get('/dashboard', verifyToken, authorizeRoles('admin'), getDashboardStats);
 
 export default router;
--- src/module/movie/movie.route.ts
+++ src/module/movie/movie.route.ts
@@ -11,12 +11,12 @@ import {
 
 const router = express.Router();
 
+router.get('/search', searchMovies);
 router.post("/", verifyToken, authorizeRoles("admin"), createMovie);
 router.get("/", getAllMovies);
 router.get("/:id", getMoviesById);
 router.put("/:id", verifyToken, authorizeRoles("admin"), updateMovie);
 router.delete("/:id", verifyToken, authorizeRoles("admin"), deleteMovie);
-router.get('/search', searchMovies);
 
 
 export default router;
